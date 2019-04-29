99625c74d6634e7f58a0a6b1d263ee91a8111964
Arturo <alopez96@ucsc.edu>
2019-04-29 09:36:34 -0700

    init

---
+++ app/src/main/java/com/squarespace/interview/android/SiteDetailsActivity.java
@@ -0,0 +1,16 @@
+package com.squarespace.interview.android;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SiteDetailsActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_site_details);
+
+    //TODO Task: Once a user clicks on a particular site, specific information of the site should be shown in this details view.
+    // Please consult the document for additional information.
+  }
+}
---
+++ app/src/main/java/com/squarespace/interview/android/SiteListActivity.java
@@ -0,0 +1,18 @@
+package com.squarespace.interview.android;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SiteListActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_site_list);
+
+    // TODO: In this activity, show a list of all the given sites.
+    // Please consult the document for additional information.
+  }
+}
+
+
